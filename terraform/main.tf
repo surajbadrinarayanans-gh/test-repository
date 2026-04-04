9c2388a98ab43662ada239d72c0aea247d95d564
surajbadrinarayanans-gh <surajbadrinarayanans@gmail.com>
2026-04-04 13:59:43 +0530

    final check

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,11 @@ resource "aws_instance" "app_server" {
   ami           = "ami-045443a70fafb8bbc"
   instance_type = "t3.micro"
 
+  # Reference the IAM profile you created earlier
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  # Add the key pair name here
+  key_name = "txn-key" 
 
   tags = {
     Name = "test-server"
